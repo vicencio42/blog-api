cca9840aae5e8db8a938db9333d30b91aaeb475d
Vicencio42 <vicencio42@gmail.com>
2022-04-20 21:30:28 -0500

    edited seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+User.create!([
+  {name: "Peter", email: "peter@test.com", password_digest: "$2a$12$px.9BcEqsHOXUO5Rat4D.eYw3pN6Eaz9PdF3QYvZLjDVUp5gyQHKC"},
+  {name: "Test", email: "test@test.com", password_digest: "$2a$12$DvX.OGj3ZgQ1CHsaywGIOeUtNg7tSncDQLLBpD3mnwDTze26Dr9kW"},
+  {name: "vicencio", email: "v@test.com", password_digest: "$2a$12$uT7uyF5J3f9XcrmM/W0GN.m.dRO0HP.QXCqYLbdhKMVHBm29ixJrS"}
+])
+
 Post.create!([
   {user_id: 1, title: "Stand-alone multi-tasking open architecture", body: "Bushwick mustache 8-bit schlitz lomo. Pour-over vice phlogiston chia hashtag authentic. Hoodie intelligentsia pitchfork single-origin coffee wolf portland. Pork belly cliche keffiyeh. Church-key organic literally loko. Kale chips wolf 8-bit. Slow-carb chartreuse pabst beard mumblecore.", image: "https://i.picsum.photos/id/302/200/300.jpg?hmac=b5e6gUSooYpWB3rLAPrDpnm8PsPb84p_NXRwD-DK-1I"},
   {user_id: 1, title: "Intuitive background implementation", body: "Trust fund butcher kinfolk austin dreamcatcher. Flexitarian twee selvage slow-carb. Cardigan banh mi put a bird on it. Portland retro tote bag vinyl butcher. Muggle magic cray tote bag locavore keytar wes anderson kale chips sartorial. Scenester trust fund whatever disrupt. Banh mi letterpress butcher swag.", image: "https://i.picsum.photos/id/131/200/300.jpg?hmac=9q7mRSOguNBFGg_gPPRKlfjNINGjXWeDBTYPP1_gEas"},
@@ -10,8 +16,4 @@ Post.create!([
   {user_id: 2, title: "Intuitive optimizing open architecture", body: "Artisan pickled austin everyday. Tattooed gluten-free muggle magic bushwick. Plaid hammock typewriter tacos wes anderson neutra you probably haven't heard of them flannel. Knausgaard tumblr xoxo salvia. Mumblecore drinking asymmetrical. Freegan leggings pickled. Kombucha farm-to-table butcher paleo tote bag etsy retro post-ironic. Yr pitchfork church-key retro.", image: "https://i.picsum.photos/id/690/200/300.jpg?hmac=YX9nONyDZ_zuGZ5wLOen_mxLWVHEsjpkADU43laON4M"},
   {user_id: 2, title: "Casa Bonita", body: "\"authentic\" mexican food", image: "https://d.newsweek.com/en/full/1775673/colorado-casa-bonita-restaurant.png?w=1600&h=1600&q=88&f=9f4551d80493cc7c41cf1ebcb75f1126"}
 ])
-User.create!([
-  {name: "Peter", email: "peter@test.com", password_digest: "$2a$12$px.9BcEqsHOXUO5Rat4D.eYw3pN6Eaz9PdF3QYvZLjDVUp5gyQHKC"},
-  {name: "Test", email: "test@test.com", password_digest: "$2a$12$DvX.OGj3ZgQ1CHsaywGIOeUtNg7tSncDQLLBpD3mnwDTze26Dr9kW"},
-  {name: "vicencio", email: "v@test.com", password_digest: "$2a$12$uT7uyF5J3f9XcrmM/W0GN.m.dRO0HP.QXCqYLbdhKMVHBm29ixJrS"}
-])
+
